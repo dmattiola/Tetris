90082b32aa6de158ded3fa2f13d37c32ace8346e
Dylan <Dylan@ap-lyonsud-principal-ss1-bu-1.univ-lyon1.fr>
2014-05-09 17:24:02 +0200

    blabla

---
+++ src/Modele/Case.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modele;
+
+/**
+ *
+ * @author Dylan
+ */
+public class Case {
+    
+    private int x;
+    private int y;
+    
+    public Case(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+}
---
+++ src/Modele/Figure.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modele;
+
+/**
+ *
+ * @author Dylan
+ */
+public class Figure {
+    
+    private Case[][] tabCase;
+    private int x;
+    private int y;
+    
+    public Figure(){
+        
+    }
+    
+    public Figure(Case[][] tabCase, int , int y){
+        
+    }
+    
+}
