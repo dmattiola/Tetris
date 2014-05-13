d11cefa87dccd75560c6dd6b511c3fdca1852ad9
Dylan <Dylan@Dylan23>
2014-05-13 14:07:32 +0200

    ...

--- src/Controleur/ControleurClavier.java
+++ src/Controleur/ControleurClavier.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Controleur;
 
 import Modele.Grille;
@@ -19,8 +13,6 @@ public class ControleurClavier implements KeyListener {
 
     Grille grille;
     Fenetre fenetre;
-    
-    public void keyTyped(KeyEvent ke) {}
 
     public void keyPressed(KeyEvent ke) {
         switch(ke.getKeyCode()){
@@ -38,5 +30,6 @@ public class ControleurClavier implements KeyListener {
 }
 
     public void keyReleased(KeyEvent ke) {}
-    
+    public void keyTyped(KeyEvent ke) {}
+
 }
--- src/Modele/Grille.java
+++ src/Modele/Grille.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Modele;
 
 import java.awt.Color;
@@ -31,7 +25,6 @@ public class Grille {
                 grille[i][j]=new Case(i,j);
             }
         }
-        System.out.println(grille);
     }
     
     
