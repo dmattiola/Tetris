c9d5bcf554e27e0e334849cf1849306126de2bfe
Dylan <dylan.mattiola@etu.univ-lyon1.fr>
2014-05-14 11:14:09 +0200

    alala

--- src/Vue/Fenetre.java
+++ src/Vue/Fenetre.java
@@ -23,31 +23,34 @@ public class Fenetre extends JFrame implements Observer{
     /**
      * Creates new form Fenetre
      */
-    public Fenetre() {
-        super();
-        initComponents();
-        //build();
-        for (int i=0;i<20;i++){
-            for(int j =0;i<10;j++){
-                plateau.add(new Case());
-            }
-        }
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent arg0) {
-                super.windowClosing(arg0);
-                System.exit(0);
-            }
-        });
-    }
-
-    public void build(){
+     public Fenetre() {
+         super();
+         build();
+         addWindowListener(new WindowAdapter() {
+             @Override
+             public void windowClosing(WindowEvent arg0) {
+                 super.windowClosing(arg0);
+                 System.exit(0);
+             }
+         });
+     }
+ 
+     public void build(){
+         JMenuBar jm = new JMenuBar();
+         JMenu m1 = new JMenu("Partie");
+         JMenu m2 = new JMenu("Help");
+         jm.add(m1);
+         jm.add(m2);
+         this.setJMenuBar(jm);
+         
+         this.setTitle("Jeu du Tetris");
+         this.setSize(450,500);
+         JComponent principal = new JPanel(new BorderLayout());
+         JComponent plateau = new JPanel(new GridLayout(20,10));
         
-        JComponent plateau = new JPanel(new GridLayout(20,10));
-       
-        Border whiteline = BorderFactory.createLineBorder(Color.white,1);
+         Border whiteline = BorderFactory.createLineBorder(Color.white,1);
         Border blackline = BorderFactory.createLineBorder(Color.black,1);
-
+ 
         for (int i = 0; i<200;i++){
             JComponent ptest = new Case();
             ptest.setBorder(whiteline);
@@ -78,57 +81,59 @@ public class Fenetre extends JFrame implements Observer{
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        principal = new javax.swing.JPanel();
-        plateau = new javax.swing.JPanel();
-        jMenuBar = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
+        MenuTouche = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+
+        MenuTouche.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        javax.swing.GroupLayout MenuToucheLayout = new javax.swing.GroupLayout(MenuTouche);
+        MenuTouche.setLayout(MenuToucheLayout);
+        MenuToucheLayout.setHorizontalGroup(
+            MenuToucheLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 428, Short.MAX_VALUE)
+        );
+        MenuToucheLayout.setVerticalGroup(
+            MenuToucheLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 455, Short.MAX_VALUE)
+        );
+
+        MenuTouche.getAccessibleContext().setAccessibleName("");
+        MenuTouche.getAccessibleContext().setAccessibleDescription("");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Jeu du Tetris");
         setPreferredSize(new java.awt.Dimension(450, 500));
         setResizable(false);
 
-        principal.setLayout(new java.awt.BorderLayout());
-
-        plateau.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        plateau.setLayout(new java.awt.GridLayout());
-        principal.add(plateau, java.awt.BorderLayout.CENTER);
-
-        jMenu1.setText("Partie");
-        jMenu1.setActionCommand("");
-        jMenuBar.add(jMenu1);
-
-        jMenu2.setText("Help");
-        jMenuBar.add(jMenu2);
-
-        setJMenuBar(jMenuBar);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(principal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addGap(0, 414, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(principal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addGap(0, 478, Short.MAX_VALUE)
         );
+
+        getAccessibleContext().setAccessibleName("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar;
-    private javax.swing.JPanel plateau;
-    private javax.swing.JPanel principal;
+    private javax.swing.JPanel MenuTouche;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 
     @Override
